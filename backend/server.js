e673ba3771cd75a23df3be1daebcad1d9be37a93
Ravip75 <praveenjat17@gmail.com>
2021-02-01 23:54:00 +0530

    d

--- backend/server.js
+++ backend/server.js
@@ -36,8 +36,7 @@ app.get("/api/config/paypal",(req,res)=>{
   //  res.send(data.products);
 //});
 
-app.use(express.static(path.join(__dirname, '/../frontend/build')));
-app.get('*', (req, res) => res.sendFile(path.join('${__dirname}/../frontend/build/index.html')));
+
 app.listen(config.PORT, () => {
   console.log('Server started at http://localhost:5000');
 });
